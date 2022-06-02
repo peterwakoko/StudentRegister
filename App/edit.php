6ef117906a7529944d6c3a033ad9f023a2f34f03
simon <peterwakoko@gmail.com>
2022-06-02 21:36:07 +0300

    updated

--- App/edit.php
+++ App/edit.php
@@ -31,7 +31,7 @@ if(isset($_GET['id'])){
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link  rel="stylesheet" href="style.css">
-   <title>Document</title>
+   <title>register</title>
 </head>
 <body>
 <form class="form-horizontal" action="update.php" method="post">
